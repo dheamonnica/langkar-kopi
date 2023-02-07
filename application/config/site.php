92bdf21747db60cf90e47933a67b48db77f4b1e8
dheamonnica <dheamonnica.acc@gmail.com>
2023-02-07 21:10:22 +0700

    push all

---
+++ application/config/site.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| Your site name,
+|
+*/
+$config['site_name'] = 'Cicool App';
+$config['version'] = '3.4.1';
